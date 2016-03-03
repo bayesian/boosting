a97b04617478da8a1577dd6de576a0aae7ab2a30
root <root@i-171582b7.inst.aws.airbnb.com>
2016-03-03 18:03:47 +0000

    add apache 2.0 liscence to the code

--- Tree.h
+++ Tree.h
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 #pragma once
 
 #include <boost/scoped_array.hpp>
@@ -44,7 +63,7 @@ class PartitionNode : public TreeNode<T> {
   double getVote() const {
     return fvote_;
   }
-  
+
   void setLeft(TreeNode<T>* left) {
     left_ = left;
   }
@@ -92,7 +111,7 @@ class PartitionNode : public TreeNode<T> {
   int fid_;
   T fv_;
   double fvote_;
-  
+
   TreeNode<T>* left_;
   TreeNode<T>* right_;
 };
@@ -140,7 +159,7 @@ TreeNode<T>* fromJson(const folly::dynamic& obj, const Config& cfg) {
   }
 
   double vote = static_cast<T>(obj["vote"].asDouble());
-  
+
   if (!feature) {
     return new LeafNode<T>(vote);
   } else {
